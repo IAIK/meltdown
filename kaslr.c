4cc3d1c381d3a7b7157c71e6fb3303988e10a071
Daniel Gruss <daniel.gruss@iaik.tugraz.at>
2018-01-08 18:32:01 +0100

    better step size

--- kaslr.c
+++ kaslr.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
   size_t scratch[4096];
   libkdump_config_t config;
   size_t offset = 0xffff880000000000ull;
-  size_t step = 0x1000000000ll;
+  size_t step = 0x800000000ll;
   size_t delta = -2 * step;
   int progress = 0;
 
